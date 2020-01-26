a325597de8aab1afa6cfd1e7f2e79ca37908b1dc
abenartzi <abenartzi@gmail.com>
2020-01-26 20:05:43 +0200

    thank you page

---
+++ bootstrap.js
@@ -0,0 +1,39 @@
+// <script>
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '{296046578021469}',
+        cookie     : true,
+        xfbml      : true,
+        version    : '{v5.0}'
+    });
+
+    FB.AppEvents.logPageView();
+
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+
+{
+    status: 'connected',
+        authResponse: {
+    accessToken: '...',
+        expiresIn:'...',
+        signedRequest:'...',
+        userID:'...'
+}
+}
+
+// </script>
